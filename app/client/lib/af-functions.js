0bb4d90bcdad020cd221c8512622f48f3ffa2ae6
Bill Gleim <bgleim@gmail.com>
2017-11-15 21:43:23 -0800

    add event tracking breadcrumbs

--- app/client/lib/af-functions.js
+++ app/client/lib/af-functions.js
@@ -6,3 +6,36 @@ web3.eth.filter('latest').watch(function(e, blockHash) {
         });
     }
 });
+
+/* to be performed on a new contract basis
+
+// Check if money arrived
+// Note checking from block 0 is non-performant
+ArtFactoryContent.Deposit({},{fromBlock: 0, toBlock: 'latest'}).watch(function(e, log) {
+    if(!e) {
+        console.log('Money arrived, from:'+ log.args.from, log.args.value.toString(10));
+        // add the transaction to our collection
+        Deposits.upsert('tx_'+ log.transactionHash ,{
+            from: log.args.from,
+            value: log.args.value.toString(10),
+            blockNumber: log.blockNumber
+        });
+    }
+});
+
+// Check if somebody set a number
+ArtFactoryContent.Publish({}).watch(function(e, log) {
+    if(!e) {
+        console.log('New content was published at block #'+ log.blockNumber);
+        alert('New content ' + log.args.art + ' was published at block #'+ log.blockNumber + ' Deposit ' + log.args.price + ' to view!');
+
+        // add the transaction to our collection
+        Publications.upsert('tx_'+ log.transactionHash ,{
+            creator: log.args.art,
+            price: log.args.price.toString(10),
+            blockNumber: log.blockNumber
+        });
+    }
+});
+
+*/
