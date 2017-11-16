e6443ff27d5d7ff3df471ff9199be52cc186ee7a
Bill Gleim <bgleim@gmail.com>
2017-11-15 19:48:35 -0800

    upgrade solc, simplify op sigs

--- contracts/ArtFactoryContent.sol
+++ contracts/ArtFactoryContent.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.15;
 
 contract ArtFactoryContent {
 	// internal variables
@@ -51,73 +51,66 @@ contract ArtFactoryContent {
   }
 
   // payment transaction via curator
-  function portalPay(address _portal) payable returns (bool) {
-    if (msg.value > 0) {
-      // 1% to the portal
-      uint portalPayAmount = msg.value/100;
-      // 99% to the creator
-      uint creatorPayAmount = msg.value - msg.value/100;
+  function portalPay(address _portal) payable {
+    // 1% to the portal
+    uint portalPayAmount = msg.value/100;
+    // 99% to the creator
+    uint creatorPayAmount = msg.value - msg.value/100;
 
-      if (portalPayAmount + creatorPayAmount != msg.value) {
-        // return funds when the creator/portal split creates an unwanted remainder
-        throw;
-      }
-
-      // send one percent to referring portal
-      // do not handle the case if send() has been overridden and fails
-      if (_portal.send(portalPayAmount) != true) throw;
-
-      // add to value associated with payer
-      amountPaid[msg.sender] += msg.value;
-
-      // if reached purchase threshold, set state to paid and re-encrypt/store content
-      if (amountPaid[msg.sender] >= price) {
-        paid[msg.sender] = true;
-
-        handles[msg.sender] = handle; // will be encrypted individually for each consumer
-      }
-
-      // add to the contract balance
-      balance += msg.value;
-
-      // send notification deposit is complete
-      Deposit(msg.sender, msg.value);
-
-      return true;
-    } else {
-      return false;
+    if (portalPayAmount + creatorPayAmount != msg.value) {
+      // return funds when the creator/portal split creates an unwanted remainder
+      revert;
     }
+
+    // send one percent to referring portal
+    // do not handle the case if send() has been overridden and fails
+    if (_portal.send(portalPayAmount) != true) revert;
+
+    // add to value associated with payer
+    amountPaid[msg.sender] += msg.value;
+
+    // if reached purchase threshold, set state to paid and re-encrypt/store content
+    if (amountPaid[msg.sender] >= price) {
+      paid[msg.sender] = true;
+
+      handles[msg.sender] = handle; // will be encrypted individually for each consumer
+    }
+
+    // add to the contract balance
+    balance += msg.value;
+
+    // send notification deposit is complete
+    Deposit(msg.sender, msg.value);
   }
 
-	function purchased() constant returns(bool purchased) {
+	function purchased() constant returns(bool) {
 		return paid[msg.sender];
 	}
 
-  function getHandle() constant returns(string handle) {
+  function getHandle() constant returns(string) {
     return handles[msg.sender];
   }
 
   // return balance
   function viewBalance() constant returns(uint) {
-    if (msg.sender != creator) throw;
+    if (msg.sender != creator) revert;
 
     return balance;
   }
 
   // withdraw complete balance
-  function withdraw() returns(uint) {
-    if (msg.sender != creator) throw;
+  function withdraw() {
+    if (msg.sender != creator) revert;
 
     // do not handle the case if send() has been overridden and fails
     if (msg.sender.send(balance)) {
       balance = 0;
     }
-    return balance;
   }
 
   // withdraw up to complete balance
-  function withdraw(uint value) returns(uint) {
-    if (msg.sender != creator) throw;
+  function withdraw(uint value) {
+    if (msg.sender != creator) revert;
 
     if (value <= balance) {
       // do not handle the case if send() has been overridden and fails
@@ -125,6 +118,5 @@ contract ArtFactoryContent {
         balance -= value;
       }
     }
-    return balance;
   }
 }
