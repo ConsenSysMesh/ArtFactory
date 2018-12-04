8ce42ce5adf5df68d764d3636510e81770520e6e
Huy <huy@gohuygo.com>
2018-12-04 15:44:17 -0800

    Uncomment ropsten network in truffle.js

--- truffle.js
+++ truffle.js
@@ -5,11 +5,11 @@ var mnemonic = "spell reduce bike mind cereal decide barely alone sport gather s
 
 module.exports = {
   networks: {
-    // ropsten: {
-    //   provider: function() { return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey); },
-    //   network_id: 3, // Official Ethereum test network
-    //   gas: 1300000
-    // },
+    ropsten: {
+      provider: function() { return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey); },
+      network_id: 3, // Official Ethereum test network
+      gas: 1300000
+    },
     development: {
       host: "localhost",
       port: 8545,
