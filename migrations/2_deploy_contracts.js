28114f6c272143a1aa889bdedf6f5539b3efa068
Huy <huy@gohuygo.com>
2018-12-04 15:54:11 -0800

    update migratio script to deploy ArtFactoryBuilder

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
-var ArtFactoryContent = artifacts.require("./ArtFactoryContent.sol");
+var ArtFactoryBuilder = artifacts.require("./ArtFactoryBuilder.sol");
 
-name = 'Symphony01'
-price = 2
-handle = 'smooth'
+
 
 module.exports = function(deployer) {
-  deployer.deploy(ArtFactoryContent, name, price, handle);
+  deployer.deploy(ArtFactoryBuilder);
 };
 
